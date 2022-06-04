089dbcebd1ea72d24d60fb6d7ec4746235a896e2
Fadli Nur Zaman <fadhlynz@gmail.com>
2022-06-05 01:11:14 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2F8F9D",
+        dark: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+};
